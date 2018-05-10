b508ce0bbe6aad6fc65f342517f477d47df073e5
hajime.shoji <shoji@i2ai.co.jp>
2018-05-10 11:03:23 +0900

    gulpインストール

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var webpackStream = require('webpack-stream');
+var webpack = require('webpack');
+
+const webpackConfig = require('./webpack.config');
+
+//gulp webpackで実行
+gulp.task('webpack', function() {
+  return webpackStream(webpackConfig, webpack)
+  .pipe(gulp.dest('./')); 
+});
